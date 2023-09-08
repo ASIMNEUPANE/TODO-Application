be0384d5078e0261622e6bceda6547e787771c9a
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-09-09 02:38:00 +0545

    updating array in react

--- client/src/Demo.js
+++ client/src/Demo.js
@@ -1,108 +1,88 @@
 import { useEffect, useState } from "react";
 import useFetchApi from "./hooks/useFetchApi";
 
-const Demo = ()=> {
-  const [car, setCar]= useState({
-    name: "Ford",
-    model:2012,
-    tires:5,
-  });
-const handleClick=()=>{
-  
-setCar((car)=>{
+const Demo = () => {
+  const [car, setCar] = useState([
+    {
+      name: "Ford",
+      model: 2012,
+      tires: 5,
+    },
+    {
+      name: "Tesla",
+      model: 2022,
+      tires: 5,
+    },
+  ]);
+  const handleArrayClick = ({id, model, name}) => {
+ 
+   
+    setCar((car)=>{
+      const newArr = [...car];
+      newArr[id] = { ...newArr[id], model, name };
+      return newArr;
+    });
+  };
 
-return {...car, name:"Tesla"}
-});
-  
-}
-
-  return(
+  return (
     <>
-    <h1>I have car name {car.name} and its model is {car.model} with {car.tires} tires</h1>
-    {/* <h1>I have selected {color? "true":"false"} color</h1> ----  TO update boolean */}
-    <br/>
-    <input type="text" id="input"/>
-    <br/>
-    <button onClick={handleClick}>Change color</button>
+      <h1>
+        I have car name {car.name} and its model year is {car.model}
+      </h1>
+      {/* <h1>I have selected {color? "true":"false"} color</h1> ----  TO update boolean */}
+      <br />
+
+      <input type="text" id="inputModel" placeholder="model" />
+      <button
+        onClick={() => handleArrayClick({ id: 1,model:2010,name:"fesla" })
+        }
+      >
+        Change name of the 2nd array
+      </button>
     </>
   );
-
-
 };
 export default Demo;
 
-
-
-
-
-
-
-
-
-
-
-
-
-//   const {
-//     isLoading,
-//     error,
-//     data: posts,
-//   } = useFetchApi({
-//     url: "https://jsonplaceholder.typicode.com/todos/1",
-//   });
-
-//   if (isLoading)
-//     return (
-//       <>
-//         <Loading />
-//       </>
-//     );
-
-//   if (error) return <>{JSON.stringify(error)}</>;
-//   return <></>;
-
-//   useEffect(() => {
-//     setTimeout(() => {});
-//   });
-// }
-
-// const [count, setCount] = useState(0);
-
-// useEffect(() => {
-//   const timer = setTimeout(() => {
-//     setCount((count) => count + 1);
-//   }, 1000);
-//   return () => clearTimeout(timer);
-// });
-
-// return <>I am {count} count </>;
-
-// const Demo = () => {
-//   const [car, setCar] = useState([
+//  ----Updating Object in react-------
+// const Demo = ()=> {
+//   const [car, setCar]= useState([
 //     {
 //     name: "Ford",
-//     model: "2000",
-//     color: "red",
+//     model:2012,
+//     tires:5,
 //   },
-//  {
-//     name: "BMW",
-//     model: "2001",
-//     color: "yellow",
+//     {
+//     name: "Tesla",
+//     model:2012,
+//     tires:5,
 //   }
+
 // ]);
+// const handleClick=(model,name)=>{
+// if(!model&& !name){
+//   alert("enter")
+// }
 
-//   const handleClick = (name,model) => {
-//     setCar((Car) => {
-//       return { ...car, name:name, model: +model };
-//     });
-//   };
+// else{
+// setCar((car)=>{
 
-//   return (
+// return {...car, model: +model,name:name }
+// });
+// }
+// }
+
+//   return(
 //     <>
-//       I have {car.name} car and it model is {car.model}
-//       <br />
-//       <input type="text" id="fname" name="fname" />
-//       <button onClick={handleClick}>Change ME</button>
+//     <h1>I have car name {car.name} and its model is {car.model} with {car.tires} tires</h1>
+//     {/* <h1>I have selected {color? "true":"false"} color</h1> ----  TO update boolean */}
+//     <br/>
+//     <input type="text" id="input" placeholder="carsName"/>
+
+//     <input type="text" id="inputModel" placeholder="model"/>
+//     <button onClick={()=>handleClick(document.getElementById('inputModel').value,document.getElementById('input').value)}>Change color</button>
 //     </>
 //   );
+
 // };
+// export default Demo;
