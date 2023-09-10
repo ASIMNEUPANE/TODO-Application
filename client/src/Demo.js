0e11f09d917515db7db46e87b1a71b44fecbbd41
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-09-10 14:01:47 +0545

    made a timer

--- client/src/Demo.js
+++ client/src/Demo.js
@@ -1,64 +1,88 @@
 import { useEffect, useState } from "react";
 import useFetchApi from "./hooks/useFetchApi";
 
-const Demo = () => {
-  const [car, setCar] = useState([
-    {
-      name: "Ford",
-      model: 2012,
-      tires: 5,
-    },
-    {
-      name: "Tesla",
-      model: 2022,
-      tires: 5,
-    },
-  ]);
-  const handleArrayClick = ({id, model, name}) => {
- 
-   
-    setCar((car)=>{
-      const newArr = [...car];
-      newArr[id] = { ...newArr[id], model, name };
-      return newArr;
-    });
-  };
+function Demo() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count)=> count+1);
+    }, 1000);
+  },[count]);
 
-  return (
-    <>
-      <h1>
-        I have car name {car.name} and its model year is {car.model}
-      </h1>
-      {/* <h1>I have selected {color? "true":"false"} color</h1> ----  TO update boolean */}
-      <br />
+  return <>I have {count} count</>;
+}
 
-      <input type="text" id="inputModel" placeholder="model" />
-      <button
-        onClick={() => handleArrayClick({ id: 1,model:2010,name:"fesla" })
-        }
-      >
-        Change name of the 2nd array
-      </button>
-    </>
-  );
-};
 export default Demo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----Updating Array using StateHooks--------
+
+// const Demo = () => {
+//   const [car, setCar] = useState([
+//     {
+//       name: "Ford",
+//       model: 2012,
+//       tires: 5,
+//     },
+//     {
+//       name: "Tesla",
+//       model: 2022,
+//       tires: 5,
+//     },
+//   ]);
+//   const handleArrayClick = ({id, model, name}) => {
+
+//     setCar((car)=>{
+//       const newArr = [...car];
+//       newArr[id] = { ...newArr[id], model, name };
+//       return newArr;
+//     });
+//   };
+
+//   return (
+//     <>
+//       <h1>
+//         I have car name {car.name} and its model year is {car.model}
+//       </h1>
+//       {/* <h1>I have selected {color? "true":"false"} color</h1> ----  TO update boolean */}
+//       <br />
+
+//       <input type="text" id="inputModel" placeholder="model" />
+//       <button
+//         onClick={() => handleArrayClick({ id: 1,model:2010,name:"fesla" })
+//         }
+//       >
+//         Change name of the 2nd array
+//       </button>
+//     </>
+//   );
+// };
+// export default Demo;
 
 //  ----Updating Object in react-------
 // const Demo = ()=> {
-//   const [car, setCar]= useState([
+//   const [car, setCar]= useState(
 //     {
 //     name: "Ford",
 //     model:2012,
 //     tires:5,
 //   },
-//     {
-//     name: "Tesla",
-//     model:2012,
-//     tires:5,
-//   }
-
-// ]);
+//
 // const handleClick=(model,name)=>{
 // if(!model&& !name){
 //   alert("enter")
